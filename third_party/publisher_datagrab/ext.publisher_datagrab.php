b4fd1333ebb710ff30f457fd1781fa37f3c8820b
Brian Litzinger <litzinger@gmail.com>
2014-03-23 18:55:25 -0500

    first

---
+++ third_party/publisher_datagrab/ext.publisher_datagrab.php
@@ -0,0 +1,160 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine Publisher Datagrab Extension Class
+ *
+ * @package     ExpressionEngine
+ * @subpackage  Extension
+ * @category    Publisher
+ * @author      Brian Litzinger
+ * @copyright   Copyright (c) 2012, 2013 - Brian Litzinger
+ * @link        http://boldminded.com/add-ons/publisher
+ * @license
+ *
+ * Copyright (c) 2012, 2013. BoldMinded, LLC
+ * All rights reserved.
+ *
+ * This source is commercial software. Use of this software requires a
+ * site license for each domain it is used on. Use of this software or any
+ * of its source code without express written permission in the form of
+ * a purchased commercial or other license is prohibited.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * As part of the license agreement for this software, all modifications
+ * to this source must be submitted to the original author for review and
+ * possible inclusion in future releases. No compensation will be provided
+ * for patches, although where possible we will attribute each contribution
+ * in file revision notes. Submitting such modifications constitutes
+ * assignment of copyright to the original author (Brian Litzinger and
+ * BoldMinded, LLC) for such modifications. If you do not wish to assign
+ * copyright to the original author, your license to  use and modify this
+ * source is null and void. Use of this software constitutes your agreement
+ * to this clause.
+ */
+
+class Publisher_datagrab_ext {
+
+    public $settings        = array();
+    public $description     = 'Adds Datagrab support to Publisher';
+    public $docs_url        = '';
+    public $name            = 'Publisher - Datagrab Support';
+    public $settings_exist  = 'n';
+    public $version         = '1.0';
+
+    /**
+     * Constructor
+     *
+     * @param   mixed   Settings array or empty string if none exist.
+     */
+    public function __construct($settings = '')
+    {
+    }
+
+    public function ajw_datagrab_pre_import($datagrab)
+    {
+        // Create cache
+        ee()->session->cache['publisher_datagrab'] = array('entry_ids' => array());
+        $this->cache =& ee()->session->cache['publisher_datagrab'];
+    }
+
+    public function ajw_datagrab_modify_data($item)
+    {
+        // var_dump($item); die;
+        // $this->cache['entry_ids'][] = $item['entry_id'];
+    }
+
+    public function ajw_datagrab_post_import($datagrab)
+    {
+        return;
+        var_dump($datagrab->entries); die;
+        if (empty($this->cache['entry_ids']))
+        {
+            return;
+        }
+
+        $where = array(
+            'publisher_lang_id' => $this->default_language_id,
+            'publisher_status' => ee()->publisher_setting->get('default_view_status')
+        );
+
+        ee()->load->model('publisher_relationships');
+        ee()->load->model('publisher_category');
+
+        ee()->publisher_entry->migrate_data($this->cache['entry_ids'], $where);
+        ee()->publisher_relationships->migrate_data($this->cache['entry_ids'], $where);
+        ee()->publisher_category->migrate_data($this->cache['entry_ids'], $where);
+    }
+
+    /**
+     * Activate Extension
+     *
+     * @return void
+     */
+    public function activate_extension()
+    {
+        // Setup custom settings in this array.
+        $this->settings = array();
+
+        // Add new hooks
+        $ext_template = array(
+            'class'    => __CLASS__,
+            'settings' => serialize($this->settings),
+            'priority' => 5,
+            'version'  => $this->version,
+            'enabled'  => 'y'
+        );
+
+        $extensions = array(
+            array('hook'=>'ajw_datagrab_pre_import', 'method'=>'ajw_datagrab_pre_import'),
+            array('hook'=>'ajw_datagrab_modify_data', 'method'=>'ajw_datagrab_modify_data'),
+            array('hook'=>'ajw_datagrab_post_import', 'method'=>'ajw_datagrab_post_import')
+        );
+
+        foreach($extensions as $extension)
+        {
+            ee()->db->insert('extensions', array_merge($ext_template, $extension));
+        }
+    }
+
+    // ----------------------------------------------------------------------
+
+    /**
+     * Disable Extension
+     *
+     * This method removes information from the exp_extensions table
+     *
+     * @return void
+     */
+    function disable_extension()
+    {
+        ee()->db->where('class', __CLASS__);
+        ee()->db->delete('extensions');
+    }
+
+    // ----------------------------------------------------------------------
+
+    /**
+     * Update Extension
+     *
+     * This function performs any necessary db updates when the extension
+     * page is visited
+     *
+     * @return  mixed   void on update / false if none
+     */
+    function update_extension($current = '')
+    {
+        if ($current == '' OR $current == $this->version)
+        {
+            return FALSE;
+        }
+    }
+
+    // ----------------------------------------------------------------------
+}
+
+/* End of file ext.publisher_datagrab.php */
+/* Location: /system/expressionengine/third_party/publisher_datagrab/ext.publisher_datagrab.php */
